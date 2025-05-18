c411caa83175a27b0c801e8ab28ac98591cc6e12
JSTKaffie <tom@G16TOM.>
2025-05-18 13:23:34 +0200

    aze

--- build-en-start-op-poort-81.sh
+++ build-en-start-op-poort-81.sh
@@ -3,7 +3,7 @@ echo "Docker-image bouwen..."
 docker buildx build   -t demo-site .
 
 echo "Container starten op poort 81..."
-docker run -d -p 81:80 --name demo-container demo-site
+docker run -d -t -p 81:80 --name demo-container demo-site
 
 echo "Demo-site is nu bereikbaar op http://localhost:81/"
 date
